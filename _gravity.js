9ebf44a999bfd2d51351a0fa62886239916ebda2
Elliot Winkler <elliot.winkler@gmail.com>
2010-09-26 19:39:37 -0600

    Make gravity a mixin

---
+++ _gravity.js
@@ -0,0 +1,47 @@
+var Gravity = {
+  mixin: function(args) {
+    args.canvasClass.prototype.objectGravity = function(obj) {
+      var newvel = obj.vel.slice(0);
+      var balls = args.objects.call(this);
+      $.each(balls, function() {
+        if (this == obj) return;
+        var f = this.gravityForce(obj);
+        newvel = Vector.add(newvel, f);
+      })
+      newvel = Vector.limit(newvel, [20, 20]);
+      var newpos = Vector.add(obj.pos, newvel);
+      return [newpos, newvel];
+    }
+    
+    $.each(args.objectClasses, function(i, klass) {
+      klass.prototype.init = (function(original) {
+        return function() {
+          original.apply(this, arguments);
+          this.mass = this.options.mass;
+          this.forceDirection = (this.options.type == "repulsive") ? -1 : 1;
+        }
+      })(klass.prototype.init);
+      
+      klass.prototype.draw = (function(original) {
+        return function() {
+          var vectors = this.canvas.objectGravity(this);
+          this.pos = vectors[0];
+          this.vel = vectors[1];
+          original.apply(this, arguments);
+        }
+      })(klass.prototype.draw);
+      
+      // FAIL: If this mass is smaller, then the gforce is smaller
+      // Is that supposed to happen?
+      klass.prototype.gravityForce = function(ball2) {
+        var G = 6.67428 * Math.pow(10, -11)
+        var r = Vector.distance(this.pos, ball2.pos);
+        var F = this.forceDirection * G * (this.mass * ball2.mass) / Math.pow(r, 2);
+        // http://www.richardsimoes.com/gravity.html
+        var Fx = (this.pos[0] - ball2.pos[0]) / r * F;
+        var Fy = (this.pos[1] - ball2.pos[1]) / r * F;
+        return [Fx, Fy];
+      }
+    })
+  }
+};
--- gravity.js
+++ gravity.js
@@ -10,20 +10,15 @@ var Ball = Class.extend({
   init: function(canvas, options) {
     this.canvas = canvas;
     this.cxt = canvas.cxt;
-    this.mass = options.mass;
+    this.options = options;
     this.radius = options.radius;
     this.pos = options.pos || this.canvas.randomPos(this.radius * 4);
     this.vel = [0, 0];
     //this.speed = options.speed || 10;
     //this.vel = options.vel || this.canvas.randomVel(this.speed);
-    this.forceDirection = (options.type == "repulsive") ? -1 : 1;
     this.color = options.color || "black";
   },
   draw: function() {
-    var vectors = this.canvas.objectGravity(this);
-    this.pos = vectors[0];
-    this.vel = vectors[1];
-    
     //var vectors = this.canvas.collision(this);
     //this.pos = vectors[0];
     //this.vel = vectors[1];
@@ -47,43 +42,22 @@ var Ball = Class.extend({
       [pos[0] - this.radius, pos[0] + this.radius],
       [pos[1] - this.radius, pos[1] + this.radius]
     ]
-  },
-  // FAIL: If this mass is smaller, then the gforce is smaller
-  // Is that supposed to happen?
-  gravityForce: function(ball2) {
-    var G = 6.67428 * Math.pow(10, -11)
-    var r = Vector.distance(this.pos, ball2.pos);
-    var F = this.forceDirection * G * (this.mass * ball2.mass) / Math.pow(r, 2);
-    // http://www.richardsimoes.com/gravity.html
-    var Fx = (this.pos[0] - ball2.pos[0]) / r * F;
-    var Fy = (this.pos[1] - ball2.pos[1]) / r * F;
-    return [Fx, Fy];
   }
 })
 Ball.generate = function(canvas, options) {
+  var m = Math.rand(1, 4)
+  var r = 3 + ((m - 1) / (4 - 1)) * (10 - 3); // 1-4 => 3-10
   var options = $.extend({
-    radius: Math.rand(5, 10),
-    mass: Math.rand(1, 4) * Math.pow(10, 6)
+    radius: r,
+    mass: m * Math.pow(10, 6)
   }, options);
   return new this(canvas, options);
 }
-
-/*
-var BallCollection = {};
-BallCollection.create = function(canvas) {
-  var collection = [];
-  collection.add = function(options) {
-    this.push(new Ball(canvas, options));
-  }
-  return collection;
-}
-*/
 
 var GravityCanvas = Canvas.extend({
   init: function(id) {
     this._super(id);
     this.frameRate = 20;
-    // this.balls = BallCollection.create(this);
     this.balls = [];
     var ball = Ball.generate(this, {
       type: "attractive",
@@ -133,15 +107,11 @@ var GravityCanvas = Canvas.extend({
     return [mx, my];
   },
   generateMouseBall: function(mpos) {
-    var m = Math.rand(1, 4)
-    var r = 3 + ((m - 1) / (4 - 1)) * (10 - 3); // 1-4 => 3-10
-    return new Ball(this, {
-      radius: r,
-      mass: m * Math.pow(10, 6),
+    return Ball.generate(this, {
       type: "attractive",
       pos: mpos,
       color: "rgba(0,0,0,0.4)"
-    });
+    })
   },
   collision: function(obj) {
     /* Calculate new displacement */
@@ -191,20 +161,14 @@ var GravityCanvas = Canvas.extend({
     if (ob[1][0] == bb[1][0] || ob[1][1] == bb[1][1]) newvel[1] = newvel[1] * -1;
     
     return [newpos, newvel];
-  },
-  objectGravity: function(obj) {
-    var newvel = obj.vel.slice(0);
-    var balls = this.balls;
-    $.each(balls, function() {
-      if (this == obj) return;
-      var f = this.gravityForce(obj);
-      newvel = Vector.add(newvel, f);
-    })
-    newvel = Vector.limit(newvel, [20, 20]);
-    var newpos = Vector.add(obj.pos, newvel);
-    return [newpos, newvel];
   }
 })
+
+Gravity.mixin({
+  canvasClass: GravityCanvas,
+  objectClasses: [Ball],
+  objects: function() { return this.balls }
+});
 
 $(function() {
   var canvas = new GravityCanvas("#canvas");
