04edcf3e9d6f321c672fd5dcd83f2470d7a4a969
Elliot Winkler <elliot.winkler@gmail.com>
2010-10-23 14:32:29 -0600

    [easing] Use current frame instead of calculating elapsed time so it's faster

--- scripts/easing.js
+++ scripts/easing.js
@@ -18,21 +18,22 @@ function()
   EasingCanvas.Object = Drawable.extend({
     init: function(canvas, options) {
       this._super(canvas);
+      this.options = options;
       this.radius = options.radius;
-      this.startPos = 200;
-      this.endPos = 400;
-      this.distance = this.endPos - this.startPos;
-      this.duration = 2;// * this.canvas.options.fps; // number of frames
-      this.pos = this.startPos;
+      this.distance = options.endPos - options.startPos;
+      this.duration = options.duration * this.canvas.options.fps; // time -> frames
+      this.pos = options.startPos;
       this.vel = this.distance / this.duration;
     },
     redraw: function() {
       // yer basic linear tween
       //var pos = Vector.add(Vector.multiply(Vector.divide(this.delta, this.duration), this.frame), this.startPos);
       // x = mx + b ==> s + vt
-      var approxTimeElapsed = (this.canvas.frameNo * this.canvas.mspf) / 1000;
-      var x = this.startPos + (this.vel * approxTimeElapsed);
-      if (x <= this.endPos) {
+      //var timeElapsed = (this.canvas.frameNo * this.canvas.mspf) / 1000;
+      //var timeElapsed = ((new Date()) - this.canvas.startTime) / 1000;
+      var timeElapsed = this.canvas.frameNo;
+      var x = this.options.startPos + (this.vel * timeElapsed);
+      if (x <= this.options.endPos) {
         this.pos = x;
       } else {
         this.canvas.stop();
@@ -44,8 +45,8 @@ function()
     }
   });
 
-  var canvas = new EasingCanvas("#canvas", { fps: 20, trackFps: true, showClock: true });
-  canvas.addObject(EasingCanvas.Object, { radius: 5 });
+  var canvas = new EasingCanvas("#canvas", { fps: 30, trackFps: true, showClock: true });
+  canvas.addObject(EasingCanvas.Object, { radius: 5, startPos: 200, endPos: 400, duration: 1.5 });
 
   // make it global
   window.canvas = canvas;
